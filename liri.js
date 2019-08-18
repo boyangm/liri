4cdd301ff82987dd4f221f1c345370b65bb722a1
Boyang Matsapola <boyangbass@gmail.com>
2019-08-18 10:24:05 -0500

    bands in town running

--- liri.js
+++ liri.js
@@ -1,18 +1,43 @@
 const dotenv = require("dotenv").config();
 const keys = require("./keys.js");
+const axios = require('axios');
 
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 
 const command = process.argv[2];
+const input = process.argv.splice(3).join('_');
+
+console.log(input);
 switch(command){
     case 'spotify-this-song':
-        
+        spotify.search({ type: 'track', query: `${input}`, limit: 5 }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+        const response =data.tracks.items.map(arr =>{
+            console.log(`Song title ${arr.name}`);
+            console.log(` Album Name:${arr.album.name}`);
+            console.log(` Artist name: ${arr.artists[0].name}`);
+            console.log(`Hear the song: ${arr.href}`);
+        })
+    
+        });
+      break;
+    case 'concert-this':
+    axios.get('https://rest.bandsintown.com/artists/' + input + '/events?app_id=codingbootcamp')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+    break;
+      
+
 } 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
-  });
